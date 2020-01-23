2cb421b0f320c47bbdb576d68ef5b93a17647cf3
Recep Cetin <recep.cetin@outlook.com>
2020-01-23 16:37:02 +0100

    add example

---
+++ examples/bucket-defaults/variables.tf
@@ -0,0 +1,19 @@
+variable "gcp_project" {
+  description = "The GCP project id"
+}
+
+variable "labels" {
+  description = "Labels used in all resources"
+  type        = map(string)
+  default = {
+    manager = "terraform"
+    team    = "teamname"
+    slack   = "talk-teamname"
+    app     = "service"
+  }
+}
+
+variable "kubernetes_namespace" {
+  description = "Your kubernetes namespace"
+  default     = "default"
+}
