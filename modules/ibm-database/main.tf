2baf31c1a400f3394cdf26839c6db01eb1fa3de1
Spikra <espenblikra@outlook.com>
2020-05-07 14:35:43 +0200

    initial ibmcloud db module

---
+++ modules/ibm-database/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "ibm" {
+  region = var.region
+}
+
+data "ibm_resource_group" "default" {
+  name = "Default"
+}
+
+locals {
+  tags = [for key, value in var.labels : "${key}:${value}"]
+}
+
+resource "random_password" "db_password" {
+  length = 12
+}
+
+resource "ibm_database" "db" {
+  name              = var.db_instance_name
+  plan              = var.db_instance_plan
+  location          = var.region
+  service           = var.db_type
+  version           = var.db_version
+  resource_group_id = data.ibm_resource_group.default.id
+  tags              = local.tags
+
+  members_memory_allocation_mb = var.db_instance_memory_mb
+  members_disk_allocation_mb   = var.db_instance_disk_size_mb
+  users {
+    name     = var.application_db_user
+    password = random_password.db_password.result
+  }
+  timeouts {
+    create = var.create_timeout
+    update = var.update_timeout
+    delete = var.delete_timeout
+  }
+}
+
+resource "kubernetes_secret" "team-db-credentials" {
+  metadata {
+    name      = "${var.labels.app}-db-credentials"
+    namespace = var.kubernetes_namespace
+    labels    = var.labels
+  }
+  data = {
+    args     = ibm_database.db.connectionstrings.0.queryoptions
+    cert     = base64decode(ibm_database.postgres_db.connectionstrings.0.certbase64)
+    host     = ibm_database.db.connectionstrings.0.hosts.0.hostname
+    name     = ibm_database.db.connectionstrings.0.database
+    password = var.db_user_pw
+    port     = ibm_database.db.connectionstrings.0.hosts.0.port
+    username = var.db_user_name
+  }
+}
+
+resource "random_id" "protector" {
+  count       = var.prevent_destroy ? 1 : 0
+  byte_length = 8
+  keepers = {
+    protector = ibm_database.postgres_db.name
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
---
+++ modules/ibm-database/variables.tf
@@ -0,0 +1,87 @@
+#
+# Variables that needs a value
+#
+variable "labels" {
+  description = "Labels used in all resources"
+  type        = map(string)
+  #default = {
+  #  team    = "TEAM"
+  #  app     = "SERVICE"
+  #}
+}
+
+variable "kubernetes_namespace" {
+  description = "Your kubernetes namespace"
+}
+
+variable "db_instance_name" {
+  description = "Database instance name override (empty string = use standard convention)"
+  default     = ""
+}
+
+#
+# Variables with default value
+#
+variable "region" {
+  description = "GCP default region"
+  type        = string
+  default     = "osl01"
+}
+
+variable "application_db_user" {
+  description = "Application user for the postgres db"
+  type        = string
+  default     = "app-user"
+}
+
+variable "prevent_destroy" {
+  description = "Prevent the destruction of this postgres database"
+  type        = bool
+  default     = false
+}
+
+variable "db_type" {
+  description = "Database type such as postgres or mongodb"
+  type        = string
+  default     = "databases-for-postgresql"
+}
+
+variable "db_instance_plan" {
+  description = "The database pricing plan"
+  type        = string
+  default     = "standard"
+}
+
+variable "db_version" {
+  description = "Database version"
+  type        = string
+  default     = "12"
+}
+
+variable "db_instance_disk_size_mb" {
+  description = "DB disc size in MB"
+  type        = string
+  default     = "5120"
+}
+
+variable "db_instance_memory_mb" {
+  description = "DB memory size in MB"
+  type        = string
+  default     = "1024"
+}
+
+# Timeouts
+variable "create_timeout" {
+  description = "The optional timout that is applied to limit long database creates."
+  default     = "60m"
+}
+
+variable "delete_timeout" {
+  description = "The optional timout that is applied to limit long database deletes."
+  default     = "20m"
+}
+
+variable "update_timeout" {
+  description = "The optional timout that is applied to limit long database updates."
+  default     = "10m"
+}
