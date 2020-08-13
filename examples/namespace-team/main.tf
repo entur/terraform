b200b5cae6083610ae0d2b26d0fa913c5c96b7b5
Alexander Brevig <alexanderbrevig@gmail.com>
2020-08-13 16:06:56 +0200

    fix examples again

--- examples/namespace-team/main.tf
+++ examples/namespace-team/main.tf
@@ -6,8 +6,8 @@ terraform {
 # This will end up not creating a namespace
 # because we target master branch
 module "namespace" {
-  #source = "github.com/entur/terraform//modules/namespace"
-  source                      = "../../modules/namespace"
+  source = "github.com/entur/terraform//modules/namespace"
+  #source                      = "../../modules/namespace"
   labels                      = var.labels
   kubernetes_namespace        = var.kubernetes_namespace
   branch_environment          = false
