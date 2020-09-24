f8f96522b33bb0f56837922e02ebf28eec2ac76f
Pavloro <51367990+Pavloro@users.noreply.github.com>
2020-09-24 10:49:34 +0200

    Update ibm module (#37)
    * add tf 0.13 support
    * update ibm example

--- modules/ibm-database/main.tf
+++ modules/ibm-database/main.tf
@@ -1,5 +1,11 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.12.0"
+    }
+  }
 }
 
 provider "ibm" {
