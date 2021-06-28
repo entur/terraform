13addab1f715de6b04ceefd16ad1a022601c5629
Leif <leif.auke@entur.org>
2021-06-28 21:42:12 +0200

    upgrade with pubsub + refactoring

---
+++ modules/cf/http_function/main.tf
@@ -0,0 +1,70 @@
+locals {
+    timestamp = formatdate("YYMMDDhhmmss", timestamp())
+	root_dir = abspath("../") 
+}
+
+# Compress source code
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = "${local.root_dir}/${var.function_source}"
+  output_path = "/tmp/function-${local.timestamp}.zip"
+}
+
+# Add source code zip to bucket
+resource "google_storage_bucket_object" "zip" {
+  # Append file MD5 to force bucket to be recreated
+  name   = "source.zip#${data.archive_file.source.output_md5}"
+  bucket = "cloudfunction-deploy-tf"
+  source = data.archive_file.source.output_path
+}
+
+# Enable Cloud Functions API
+resource "google_project_service" "cf" {
+  project = var.project
+  service = "cloudfunctions.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
+
+# Enable Cloud Build API
+resource "google_project_service" "cb" {
+  project = var.project
+  service = "cloudbuild.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
+
+# Create http Cloud Function
+resource "google_cloudfunctions_function" "http_trigger" {
+  
+  project               = var.project
+  region                = var.region
+
+  name                  = var.function_name
+  runtime               = var.runtime
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket_object.zip.bucket
+  source_archive_object = google_storage_bucket_object.zip.name
+  trigger_http          = true
+  entry_point           = var.function_entry_point
+
+  depends_on = [
+      google_storage_bucket_object.zip
+    ]
+
+}
+
+# Create IAM entry so all users can invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = var.project
+  region         = var.region
+  cloud_function = var.function_name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
+
+    depends_on = [
+      google_cloudfunctions_function.http_trigger
+    ]
+}
